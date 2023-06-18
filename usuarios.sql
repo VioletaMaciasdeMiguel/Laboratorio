e886016909620672258af4ad1b1a203c25f9f6a5
VioletaMaciasdeMiguel <109521077+VioletaMaciasdeMiguel@users.noreply.github.com>
2023-06-18 18:21:06 +0200

    Update usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -3,8 +3,9 @@ CREATE TABLE `usuario` (
     `nombre` VARCHAR(20),
     `apellido1` VARCHAR(20),
     `apellido2` VARCHAR(20),
-    `email` VARCHAR(20)
+    `email` VARCHAR(20),
+    `login` VARCHAR(20),
     `contrasenia` VARCHAR(8),);
     
-INSERT INTO usuario (nombre, apellido1, apellido2, contrasenia, email, id)
-VALUES ('Antonio', 'Garcia', 'Garcia', '1234', 'antoniogarcia@gmail.com', '1');
+INSERT INTO usuario (nombre, apellido1, apellido2, contrasenia, email, login, id)
+VALUES ('Antonio', 'Garcia', 'Garcia', '1234', 'antoniogarcia@gmail.com', 'antoni', '1');
